d4019c536df8c2abc411825b4501c7a80a83fe0c
Bond_009 <bond.009@outlook.com>
2020-12-02 20:22:31 +0100

    Add day 1

---
+++ 1/part1.c
@@ -0,0 +1,33 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define INPUT_LEN 200
+
+int repair(int * arr)
+{
+    for (int i = 0; i < INPUT_LEN; i++) {
+        for (int j = 0; j < INPUT_LEN; j++) {
+            if (arr[i] + arr[j] == 2020) {
+                return arr[i] * arr[j];
+            }
+        }
+    }
+
+    return 0;
+}
+
+int main()
+{
+    FILE * file = fopen("input", "r");
+
+    char buffer[8] = { 0 };
+    int input[INPUT_LEN] = { 0 };
+    for (int i = 0; i < 200; i++) {
+        fgets(buffer, 8, file);
+        input[i] = atoi(buffer);
+    }
+
+    printf("%i", repair(input));
+}
