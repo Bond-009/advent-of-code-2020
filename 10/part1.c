c45c08811c6f41d84871f6a83a7317da042feb51
Bond_009 <bond.009@outlook.com>
2020-12-10 15:09:11 +0100

    Add day 10

---
+++ 10/part1.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <string.h>
+
+#define MAX_INPUT_LEN 128
+
+void insert_value_sorted(int *list, size_t *size, int value)
+{
+    long long low = 0, high = *size;
+    while (low < high) {
+        int m = low + (high - low) / 2;
+        if (list[m] == value) {
+            /* Name already exists,
+                return pointer to the already existsing string and free the new one.
+            */
+            return;
+        }
+        else if (list[m] < value) {
+            low = m + 1;
+        }
+        else {
+            high = m;
+        }
+    }
+
+    for (long long i = *size - 1; i >= low; i--) {
+        list[i + 1] = list[i];
+    }
+
+    (*size)++;
+    list[low] = value;
+}
+
+int bags_count(const char *filename)
+{
+    FILE *file = fopen(filename, "r");
+
+    // Include space for newline and string terminator
+    char buffer[128] = { 0 };
+
+    int input[MAX_INPUT_LEN] = { 0 };
+    size_t input_size = 1; // 0 is our start value
+
+    while (fgets(buffer, 128, file)) {
+        puts(buffer);
+        insert_value_sorted(input, &input_size, atoi(buffer));
+    }
+
+    fclose(file);
+
+    int diff1 = 0;
+    int diff3 = 1; // Diff with adapter
+
+    for (size_t i = 1; i < input_size; i++)
+    {
+        int diff = input[i] - input[i - 1];
+        if (diff == 1) {
+            diff1++;
+        }
+        else if (diff == 3) {
+            diff3++;
+        }
+    }
+
+    return diff1 * diff3;
+}
+
+int main(int argc, char *argv[])
+{
+    printf("%i\n", bags_count(argv[argc - 1]));
+}
---
+++ 10/part2.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <string.h>
+
+#define MAX_INPUT_LEN 128
+
+void insert_value_sorted(int *list, size_t *size, int value)
+{
+    long long low = 0, high = *size;
+    while (low < high) {
+        int m = low + (high - low) / 2;
+        if (list[m] == value) {
+            /* Name already exists,
+                return pointer to the already existsing string and free the new one.
+            */
+            return;
+        }
+        else if (list[m] < value) {
+            low = m + 1;
+        }
+        else {
+            high = m;
+        }
+    }
+
+    for (long long i = *size - 1; i >= low; i--) {
+        list[i + 1] = list[i];
+    }
+
+    (*size)++;
+    list[low] = value;
+}
+
+size_t bags_count(const char *filename)
+{
+    FILE *file = fopen(filename, "r");
+
+    // Include space for newline and string terminator
+    char buffer[16] = { 0 };
+
+    int input[MAX_INPUT_LEN] = { 0 };
+    size_t input_size = 1; // 0 is our start value
+
+    while (fgets(buffer, 16, file)) {
+        insert_value_sorted(input, &input_size, atoi(buffer));
+    }
+
+    // Add our device's built-in joltage adapter
+    insert_value_sorted(input, &input_size, input[input_size - 1] + 3);
+
+    fclose(file);
+
+    /* Removed the 2 first values so we don't need to add 2 to our index every time
+        Longest consecutive input is 5 */
+    const static int TRIB[] = { 1, 1, 2, 4, 7 };
+    int con = 0;
+    size_t res = 1;
+
+    for (size_t i = 1; i < input_size; i++)
+    {
+        int diff = input[i] - input[i - 1];
+        if (diff == 1) {
+            con++;
+        }
+        else {
+            res *= TRIB[con];
+            con = 0;
+        }
+    }
+
+    return res;
+}
+
+int main(int argc, char *argv[])
+{
+    printf("%zu\n", bags_count(argv[argc - 1]));
+}
